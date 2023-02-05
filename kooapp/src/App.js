1b7b9319e0b2a138b7d2116fc02b27b5ba12c0bb
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-02-05 16:25:34 +0530

    ocde

--- kooapp/src/App.js
+++ kooapp/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import practice from './Component/practice';
-
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <practice />
+      <Navbar/>
     </div>
   );
 }
 
-export default App;
+import React from 'react'
+import Navbar from './Components/Navbar';
+
+
