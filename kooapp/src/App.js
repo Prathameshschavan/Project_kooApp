48276b62ddcdeecc3a1aeeb845cccfbe5c92bf95
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-02-11 01:04:50 +0530

    cpoidE

--- kooapp/src/App.js
+++ kooapp/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import RoutesComp from './Components/RoutesComp';
 import Feed from './Components/Feed';
+import Video from './Components/Video';
 function App() {
   return (
     <div className="App">
-      <RoutesComp/>
+      {/* <RoutesComp/>
        <Navbar/>
        <div>
        <Feed/>
-       </div>
+       </div> */}
+       <Video/>
     </div>
    
   )
--- kooapp/src/Components/Video.jsx
+++ kooapp/src/Components/Video.jsx
@@ -31,7 +31,63 @@ width="300px"  height="300px"
    loop
  > something</video>
 {/* <source src={require('https://ak.picdn.net/shutterstock/videos/1031891591/preview/stock-footage-group-young-beautiful-woman-asian-running-on-a-treadmill-at-gym-fitness-and-healthy-lifestyle.mp4')} type="video/mp4" /> */}
+<br/><br/>
 
+<video
+    width="300px"  height="300px"
+   src="https://cdn.coverr.co/videos/coverr-taking-pictures-on-a-boat-7292/1080p.mp4"
+   controls
+   muted
+   autoPlay={"autoplay"}
+   preLoad="auto"
+   loop
+ > something</video>
+
+<br/><br/>
+<video
+    width="300px"  height="300px"
+   src="https://cdn.coverr.co/videos/coverr-walking-on-a-ledge-633/1080p.mp4"
+   controls
+   muted
+   autoPlay={"autoplay"}
+   preLoad="auto"
+   loop
+ > something</video>
+
+
+<br/><br/>
+<video
+    width="300px"  height="300px"
+   src="https://cdn.coverr.co/videos/coverr-friends-laughing-and-looking-at-the-camera-4194/1080p.mp4"
+   controls
+   muted
+   autoPlay={"autoplay"}
+   preLoad="auto"
+   loop
+ > something</video>
+<br/><br/>
+
+<video
+    width="300px"  height="300px"
+   src="https://cdn.coverr.co/videos/coverr-friends-laughing-and-looking-at-the-camera-4194/1080p.mp4"
+   controls
+   muted
+   autoPlay={"autoplay"}
+   preLoad="auto"
+   loop
+ > something</video>
+
+<br/><br/>
+
+<video
+    width="300px"  height="300px"
+   src="https://cdn.coverr.co/videos/coverr-friends-laughing-and-looking-at-the-camera-4194/1080p.mp4"
+   controls
+   muted
+   autoPlay={"autoplay"}
+   preLoad="auto"
+   loop
+ > something</video>
         </>
     )
 }
