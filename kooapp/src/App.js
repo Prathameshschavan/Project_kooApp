fb0918ee250a51d024a0a965d3eee0fc647ab2d5
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-02-10 10:59:41 +0530

    code

--- kooapp/src/App.js
+++ kooapp/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Navbar from './Components/Navbar';
 import RoutesComp from './Components/RoutesComp';
@@ -9,17 +8,6 @@ function App() {
       <RoutesComp/>
        <Navbar/>
     </div>
-=======
-
-import './App.css';
-import Feed from './Component/Feeds';
-import Trending from './Component/Trending/Trending';
-function App() {
-  return (
-    <div className="App">
-      <Feed/>
-   </div>
->>>>>>> e16e86a7ffada6980115f2cc3bac8db149cc759c
-  );
+  )
 }
 export default App;
